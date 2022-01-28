de18845ccf27a862c3fe583719f0546f84171ab5
Ryan Ashby <rjapsu160@gmail.com>
2022-01-28 11:29:09 -0800

    add output div with spans for cost and chosen pizza type

--- js/scripts.js
+++ js/scripts.js
@@ -10,63 +10,65 @@ function Pizza(size, [topping1, topping2, topping3], style) {
 Pizza.prototype.cost = function() {
   let cost = 0;
 
-  // if (this.size === "medium") {
-  //   cost += 9;
-  // } else if (this.size === "large") {
-  //   cost =+ 12;
-  // } else if (this.size === "extra") {
-  //   cost =+ 15;
-  // }
-  // return cost;
-  // if (this.style === "new york") {
-  //   cost += 1;
-  // } else if (this.size === "chicago") {
-  //   cost += 4;
-  // } else if (this.size === "detroit") {
-  //   cost += 3;
-  // }
-  // if (this.topping1 === "cheese") {
-  //   cost += 1;
-  // } else if (this.topping1 === "pepperoni") {
-  //   cost += 2;
-  // } else if (this.topping1 === "sausage") {
-  //   cost += 2;
-  // } else if (this.topping1 === "onion") {
-  //   cost += 1;
-  // } else if (this.topping1 === "mushroom") {
-  //   cost += 1;
-  // } else if (this.topping1 === "ham") {
-  //   cost += 2;
-  // } else if (this.topping1 === "pineapple") {
-  //   cost += 1;
-  // } else if (this.topping1 === "bacon") {
-  //   cost += 2;
-  // } else if (this.topping1 === "banana") {
-  //   cost += 1;
-  // } else {
-  //   cost += 2;
-  // }
-  // if (this.topping2 === "pepperoni") {
-  //   cost += 2;
-  // } else if (this.topping2 === "sausage") {
-  //   cost += 2;
-  // } else if (this.topping2 === "onion") {
-  //   cost += 1;
-  // } else if (this.topping2 === "mushroom") {
-  //   cost += 1;
-  // } else if (this.topping2 === "ham") {
-  //   cost += 2;
-  // } else if (this.topping2 === "pineapple") {
-  //   cost += 1;
-  // } else if (this.topping2 === "bacon") {
-  //   cost += 2;
-  // } else if (this.topping2 === "banana") {
-  //   cost += 1;
-  // } else if (this.topping2 === "hotdog") {
-  //   cost += 2;
-  // } else {
-  //   cost += 0;
-  // }
+  if (this.size === "medium") {
+    cost += 9;
+  } else if (this.size === "large") {
+    cost =+ 12;
+  } else if (this.size === "extra") {
+    cost =+ 15;
+  }
+  
+  if (this.style === "new york") {
+    cost += 1;
+  } else if (this.size === "chicago") {
+    cost += 4;
+  } else if (this.size === "detroit") {
+    cost += 3;
+  }
+
+  if (this.topping1 === "cheese") {
+    cost += 1;
+  } else if (this.topping1 === "pepperoni") {
+    cost += 2;
+  } else if (this.topping1 === "sausage") {
+    cost += 2;
+  } else if (this.topping1 === "onion") {
+    cost += 1;
+  } else if (this.topping1 === "mushroom") {
+    cost += 1;
+  } else if (this.topping1 === "ham") {
+    cost += 2;
+  } else if (this.topping1 === "pineapple") {
+    cost += 1;
+  } else if (this.topping1 === "bacon") {
+    cost += 2;
+  } else if (this.topping1 === "banana") {
+    cost += 1;
+  } else {
+    cost += 2;
+  }
+
+  if (this.topping2 === "pepperoni") {
+    cost += 2;
+  } else if (this.topping2 === "sausage") {
+    cost += 2;
+  } else if (this.topping2 === "onion") {
+    cost += 1;
+  } else if (this.topping2 === "mushroom") {
+    cost += 1;
+  } else if (this.topping2 === "ham") {
+    cost += 2;
+  } else if (this.topping2 === "pineapple") {
+    cost += 1;
+  } else if (this.topping2 === "bacon") {
+    cost += 2;
+  } else if (this.topping2 === "banana") {
+    cost += 1;
+  } else if (this.topping2 === "hotdog") {
+    cost += 2;
+  } else {
+    cost += 0;
+  }
 
   if (this.topping3 === "pepperoni") {
     cost += 2;

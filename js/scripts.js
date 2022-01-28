b58656c687058544b9d05a72e0698246b6ffc063
Ryan Ashby <rjapsu160@gmail.com>
2022-01-28 08:55:40 -0800

    pass second test with more properties to pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 // Business Logic
-function Pizza(size, [topping1, topping2]) {
+function Pizza(size, [topping1, topping2, topping3], style) {
   this.size = size;
-  this.toppings = [topping1, topping2];
+  this.toppings = [topping1, topping2, topping3];
+  this.style = style
 }
 
-const pizza = new Pizza("large", ["cheese", "pepperoni"]);
+const pizza = new Pizza("large", ["cheese", "pepperoni", "sausage"], "new york");
 console.log(pizza);
 
 

aa4ea4c5e1a6ecf2f8d38f7ef510793f160e9db7
Ryan Ashby <rjapsu160@gmail.com>
2022-01-28 11:17:51 -0800

    add business logic for toppings 2 and 3, mirroring topping 1 logic

--- js/scripts.js
+++ js/scripts.js
@@ -25,27 +25,70 @@ Pizza.prototype.cost = function() {
   // } else if (this.size === "detroit") {
   //   cost += 3;
   // }
-    if (this.topping1 === "cheese") {
-      cost += 1;
-    } else if (this.topping1 === "pepperoni") {
-      cost += 2;
-    } else if (this.topping1 === "sausage") {
-      cost += 2;
-    } else if (this.topping1 === "onion") {
-      cost += 1;
-    } else if (this.topping1 === "mushroom") {
-      cost += 1;
-    } else if (this.topping1 === "ham") {
-      cost += 2;
-    } else if (this.topping1 === "pineapple") {
-      cost += 1;
-    } else if (this.topping1 === "bacon") {
-      cost += 2;
-    } else if (this.topping1 === "banana") {
-      cost += 1;
-    } else {
-      cost += 2;
-    }
+  // if (this.topping1 === "cheese") {
+  //   cost += 1;
+  // } else if (this.topping1 === "pepperoni") {
+  //   cost += 2;
+  // } else if (this.topping1 === "sausage") {
+  //   cost += 2;
+  // } else if (this.topping1 === "onion") {
+  //   cost += 1;
+  // } else if (this.topping1 === "mushroom") {
+  //   cost += 1;
+  // } else if (this.topping1 === "ham") {
+  //   cost += 2;
+  // } else if (this.topping1 === "pineapple") {
+  //   cost += 1;
+  // } else if (this.topping1 === "bacon") {
+  //   cost += 2;
+  // } else if (this.topping1 === "banana") {
+  //   cost += 1;
+  // } else {
+  //   cost += 2;
+  // }
+  // if (this.topping2 === "pepperoni") {
+  //   cost += 2;
+  // } else if (this.topping2 === "sausage") {
+  //   cost += 2;
+  // } else if (this.topping2 === "onion") {
+  //   cost += 1;
+  // } else if (this.topping2 === "mushroom") {
+  //   cost += 1;
+  // } else if (this.topping2 === "ham") {
+  //   cost += 2;
+  // } else if (this.topping2 === "pineapple") {
+  //   cost += 1;
+  // } else if (this.topping2 === "bacon") {
+  //   cost += 2;
+  // } else if (this.topping2 === "banana") {
+  //   cost += 1;
+  // } else if (this.topping2 === "hotdog") {
+  //   cost += 2;
+  // } else {
+  //   cost += 0;
+  // }
+
+  if (this.topping3 === "pepperoni") {
+    cost += 2;
+  } else if (this.topping3 === "sausage") {
+    cost += 2;
+  } else if (this.topping3 === "onion") {
+    cost += 1;
+  } else if (this.topping3 === "mushroom") {
+    cost += 1;
+  } else if (this.topping3 === "ham") {
+    cost += 2;
+  } else if (this.topping3 === "pineapple") {
+    cost += 1;
+  } else if (this.topping3 === "bacon") {
+    cost += 2;
+  } else if (this.topping3 === "banana") {
+    cost += 1;
+  } else if (this.topping3 === "hotdog") {
+    cost += 2;
+  } else {
+    cost += 0;
+  }
   return cost;
 }
 const pizza = new Pizza("medium", ["cheese", "pepperoni", "sausage"], "new york");

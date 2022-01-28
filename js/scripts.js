38d0b14b6f001678ad04d5b789dbb4908e4e2d43
Ryan Ashby <rjapsu160@gmail.com>
2022-01-28 09:23:45 -0800

    write pizza.cost prototype to increment cost, starting at 0

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,10 @@ function Pizza(size, [topping1, topping2, topping3], style) {
 }
 
 const pizza = new Pizza("large", ["cheese", "pepperoni", "sausage"], "new york");
-console.log(pizza);
 
+Pizza.prototype.cost = function() {
+  let cost = 0;
+}
+console.log(pizza);
 
 // UI Logic

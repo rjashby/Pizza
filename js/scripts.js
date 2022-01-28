404d14c474eaf8314e3286b4acfbe5352a7f274f
Ryan Ashby <rjapsu160@gmail.com>
2022-01-28 10:54:49 -0800

    pass fifth test, incrementing cost based on style

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,20 @@ function Pizza(size, [topping1, topping2, topping3], style) {
 Pizza.prototype.cost = function() {
   let cost = 0;
 
-  if (this.size === "medium") {
-    cost += 9;
-  } else if (this.size === "medium") {
-    cost =+ 12;
-  } else if (this.size === "medium") {
-    cost =+ 15;
+  // if (this.size === "medium") {
+  //   cost += 9;
+  // } else if (this.size === "large") {
+  //   cost =+ 12;
+  // } else if (this.size === "extra") {
+  //   cost =+ 15;
+  // }
+  // return cost;
+  if (this.style === "new york") {
+    cost += 1;
+  } else if (this.size === "chicago") {
+    cost =+ 4;
+  } else if (this.size === "detroit") {
+    cost =+ 3;
   }
   return cost;
 }
